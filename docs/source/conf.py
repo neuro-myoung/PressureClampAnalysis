6056df546af860992d5dc8824ac98a07fe737a01
Michael Young <michael.young@duke.edu>
2022-01-16 18:50:07 -0500

    consistent versioning

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Michael Young'
 author = 'Michael Young'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
